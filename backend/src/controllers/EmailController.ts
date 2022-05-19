609143a7067ac7e2ae0c985155d010a1c77cff2b
pigozzi <rafaelpigozzi@gmail.com>
2022-05-19 16:33:04 -0300

    Alterações no controlador de email e whatsapp

--- backend/src/controllers/EmailController.ts
+++ backend/src/controllers/EmailController.ts
@@ -7,48 +7,53 @@ class EmailController {
 
     async sendEmail(request: Request, response: Response) {
 
-        const user_id = request.headers.authorization;
+        try {
+            const user_id = request.headers.authorization;
 
-        if (!user_id) {
-            return response.status(404).json({ error: "USER ID NOT FOUND" })
+            if (!user_id) {
+                return response.status(404).json({ error: "USER ID NOT FOUND" })
+            }
+
+            const email = await knex('users')
+                .select('email')
+                .select('fullName')
+                .where('id', user_id)
+
+            const emailVerify = email[0].email
+            const nameVerify = email[0].fullName
+
+            const transport = nodemailer.createTransport({
+                host: 'smtp.gmail.com',
+                port: 587,
+                auth: {
+                    user: 'test3.exame@gmail.com',
+                    pass: '123456rafa',
+                },
+            });
+
+            const token = Math.floor(Math.random() * 100000 + 99999);
+
+            await transport.sendMail({
+                to: `${nameVerify} <${emailVerify}>`,
+                subject: "Autenticação",
+                html: [
+                    `<div style="font-family: sans-serif; font-size: 16px; colors: #111;">`,
+                    `<p> Olá, seja bem vindo!</p>`,
+                    `<p>Sua chave secreta é: ${token}</p>`,
+                    `</div>`
+                ].join('\n')
+            })
+
+            await knex('keys').insert({
+                user_id,
+                token
+            });
+
+            return response.status(201).json({ message: "E-mail enviado com sucesso!" });
+        } catch (err) {
+            return response.json({ message: "Não foi possivel enviar o e-mail" })
         }
 
-        const email = await knex('users')
-            .select('email')
-            .select('fullName')
-            .where('id', user_id)
-
-        const emailVerify = email[0].email
-        const nameVerify = email[0].fullName
-
-        const transport = nodemailer.createTransport({
-            host: 'smtp.gmail.com',
-            port: 587,
-            auth: {
-                user: 'test3.exame@gmail.com',
-                pass: '123456rafa',
-            },
-        });
-
-        const token = Math.floor(Math.random() * 100000 + 99999);
-
-        await transport.sendMail({
-            to: `${nameVerify} <${emailVerify}>`,
-            subject: "Autenticação",
-            html: [
-                `<div style="font-family: sans-serif; font-size: 16px; colors: #111;">`,
-                `<p> Olá, seja bem vindo!</p>`,
-                `<p>Sua chave secreta é: ${token}</p>`,
-                `</div>`
-            ].join('\n')
-        })
-
-        await knex('keys').insert({
-            user_id,
-            token
-        });
-
-        return response.status(201).json({ message: "E-mail enviado com sucesso!" });
     }
 
 }
--- backend/src/controllers/WhatsAppController.ts
+++ backend/src/controllers/WhatsAppController.ts
@@ -7,39 +7,46 @@ class WhatsAppController {
 
     async sendToken(request: Request, response: Response) {
 
-        const user_id = request.headers.authorization;
+        try {
+            const user_id = request.headers.authorization;
 
-        if (!user_id) {
-            return response.status(404).json({ error: "USER ID NOT FOUND" })
-        }
-
-        const phone = await knex('users')
-            .select('phone')
-            .where('id', user_id)
-
-        const token = Math.floor(Math.random() * 100000 + 99999);
-
-        const phoneVerify = phone[0].phone
-
-        await axios.post(
-            'https://api.z-api.io/instances/3AB9B618169E601D04BAFE0E2C9738F8/token/B7D2B00D13806F11C2405772/send-messages',
-            {
-                "phone": `+55${phoneVerify}`,
-                "message": `Seu código de verificação é: ${token}`
+            if (!user_id) {
+                return response.status(404).json({ error: "USER ID NOT FOUND" })
             }
-        ).then(() => {
-            console.log('Mensagem enviada com sucesso!')
-        }).catch(err => {
-            console.log(err)
-        })
 
-        await knex('keys').insert({
-            user_id,
-            token
-        });
+            const phone = await knex('users')
+                .select('phone')
+                .where('id', user_id)
 
-        return response.json({ message: "Enviado com sucesso" })
+            const token = Math.floor(Math.random() * 100000 + 99999);
+
+            const phoneVerify = phone[0].phone
+
+            await axios.post(
+                'https://api.z-api.io/instances/3AB9B618169E601D04BAFE0E2C9738F8/token/B7D2B00D13806F11C2405772/send-messages',
+                {
+                    "phone": `+55${phoneVerify}`,
+                    "message": `Seu código de verificação é: ${token}`
+                }
+            ).then(() => {
+                console.log('Mensagem enviada com sucesso!')
+            }).catch(err => {
+                console.log(err)
+            })
+
+            await knex('keys').insert({
+                user_id,
+                token
+            });
+
+            return response.json({ message: "Enviado com sucesso" })
+
+        } catch (err) {
+            return response.json({ message: "Não foi possivel enviar o token" })
+        }
     }
+
+
 }
 
 export default WhatsAppController;
